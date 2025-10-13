79a9985acb833e79b29393fa2f6a9014a7250e86
runninyeti <brock@indexing.co>
2025-10-13 21:23:29 +0200

    fix: ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
+import * as allTypes from './types';
+import * as allUtils from './utils';
 import { templates as templateList } from './templates';
 
-export * as utils from './utils';
-export * as types from './types';
-
+export type Template = allTypes.Template;
+export const utils = { ...allUtils };
 export const templates = templateList.reduce((a, b) => ({ ...a, [b.key]: Object.assign({}, b) }), {});
 
 export function getAllTemplates() {
   return templateList.slice();
 }
 
-//Function to get a template by its key
+// Function to get a template by its key
 export function getTemplateByKey(key: string) {
   return templateList.slice().find((template) => template.key === key);
 }
