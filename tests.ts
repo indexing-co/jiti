8de309f1450f202c753f70ac5c04a4def01d87ee
runninyeti <brock@indexing.co>
2025-12-16 16:20:12 -0600

    Revert "update package version"
    This reverts commit 4e9571f9794658b97afa7c263ee866785e52a65c.

--- tests.ts
+++ tests.ts
@@ -1,13 +1,6 @@
 import assert from 'assert';
-import fs from 'fs';
-
-process.env.IS_JITI_TEST = 'false';
 
 import { templates } from './src/index';
-
-BigInt.prototype['toJSON'] = function () {
-  return this.toString();
-};
 
 async function runTests() {
   for (const key in templates) {
@@ -17,10 +10,7 @@ async function runTests() {
 
     console.log('Running tests for', key);
     for (const test of templates[key].tests) {
-      if (key !== 'filterValues') continue;
-      if (typeof test.payload !== 'string' || !test.payload.includes('hyper_core')) continue;
       console.log('->', typeof test.payload === 'string' ? test.payload : JSON.stringify(test.params));
-      let outputPath = '';
 
       try {
         const payload =
@@ -35,16 +25,9 @@ async function runTests() {
         }
 
         const output = templates[key].transform(payload, { params: test.params });
-        outputPath = `tmp/${key}-${(test.payload as string).split('/').pop()}.json`;
-        fs.writeFileSync(
-          `tmp/${key}-${(test.payload as string).split('/').pop()}.json`,
-          JSON.stringify(output, null, 2)
-        );
         assert.deepStrictEqual(output, test.output);
       } catch (e) {
         console.error(e);
-        console.log(outputPath);
-        break;
       }
     }
   }
